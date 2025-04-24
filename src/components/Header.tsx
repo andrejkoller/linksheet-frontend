0b9780b279712b34329d2bb91dca505bfb8462b9
andrejkoller <andrejkoller@outlook.com>
2025-04-24 20:20:54 +0200

    feat: Implement menu component with overlay and close functionality for improved navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,46 +1,85 @@
 import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+import Menu from "./Menu";
 
 const Header = () => {
+  const menuOverlayRef = useRef<HTMLDivElement>(null);
+  const menuRef = useRef<HTMLDivElement>(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const handleMenuOpen = () => {
+    setIsMenuOpen(true);
+    document.body.style.overflow = "hidden";
+  };
+
+  const handleMenuClose = () => {
+    setIsMenuOpen(false);
+    document.body.style.overflow = "auto";
+  };
+
   return (
-    <div className="header">
-      <div className="header-container">
-        <div className="header-content">
-          <div className="header-left">
-            <div className="header-left-content">
-              <div className="header-left-content-item">
-                <div className="header-logo">
-                  <h1>
-                    <Link to={"/"}>Linksheet</Link>
-                  </h1>
+    <>
+      <div className="header">
+        <div className="header-container">
+          <div className="header-content">
+            <div className="header-left">
+              <div className="header-left-content">
+                <div className="header-left-content-item">
+                  <div className="header-logo">
+                    <h1>
+                      <Link to={"/"}>Linksheet</Link>
+                    </h1>
+                  </div>
+                </div>
+                <div className="header-left-content-item">
+                  <Link to={"/templates"}>Templates</Link>
+                </div>
+                <div className="header-left-content-item">
+                  <Link to={"/discover"}>Discover</Link>
+                </div>
+                <div className="header-left-content-item">
+                  <Link to={"/learn"}>Learn</Link>
                 </div>
               </div>
-              <div className="header-left-content-item">
-                <Link to={"/templates"}>Templates</Link>
-              </div>
-              <div className="header-left-content-item">
-                <Link to={"/discover"}>Discover</Link>
-              </div>
-              <div className="header-left-content-item">
-                <Link to={"/learn"}>Learn</Link>
-              </div>
             </div>
-          </div>
-          <div className="header-right">
-            <div className="header-right-content">
-              <div className="header-right-content-item">
-                <Link to={"/login"}>Log in</Link>
-              </div>
-              <div className="header-right-content-item">
-                <Link to={"/register"}>Sign up free</Link>
-              </div>
-              <div className="header-menu-icon">
-                <i className="fa-solid fa-bars"></i>
+            <div className="header-right">
+              <div className="header-right-content">
+                <div className="header-right-content-item">
+                  <Link to={"/login"}>Log in</Link>
+                </div>
+                <div className="header-right-content-item">
+                  <Link to={"/register"}>Sign up free</Link>
+                </div>
+                <div
+                  id="menuButton"
+                  className="header-menu-icon"
+                  onClick={handleMenuOpen}
+                >
+                  <i className="fa-solid fa-bars" aria-hidden="true"></i>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <div
+        ref={menuOverlayRef}
+        className={`menu-overlay ${isMenuOpen ? "menu-overlay-open" : ""}`}
+        style={{
+          backgroundColor: isMenuOpen ? "rgba(0, 0, 0, 0.5)" : "transparent",
+          zIndex: isMenuOpen ? 1000 : -1,
+          height: isMenuOpen ? "100vh" : "0vh",
+        }}
+        onClick={handleMenuClose}
+      >
+        <div
+          ref={menuRef}
+          className={`menu ${isMenuOpen ? "menu-open" : "menu-close"}`}
+        >
+          <Menu />
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ src/components/Menu.tsx
@@ -0,0 +1,68 @@
+import { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+
+const Menu = () => {
+  const location = useLocation();
+
+  const handleMenuClose = () => {
+    const menuOverlay = document.getElementById("menuOverlay");
+    const menu = document.getElementById("menu");
+
+    if (menuOverlay && menu) {
+      menuOverlay.style.backgroundColor = "transparent";
+      menuOverlay.style.zIndex = "-1";
+      menuOverlay.style.height = "0vh";
+      menu.classList.remove("menu-open");
+      menu.classList.add("menu-close");
+    }
+  };
+
+  useEffect(() => {
+    handleMenuClose();
+  }, [location.pathname]);
+
+  return (
+    <div className="menu-container">
+      <div className="menu-content">
+        <div className="menu-header">
+          <div className="menu-title">
+            <h1>Menu</h1>
+          </div>
+          <div
+            className="menu-close-icon"
+            id="menuCloseIcon"
+            onClick={handleMenuClose}
+          >
+            <i className="fa-solid fa-xmark"></i>
+          </div>
+        </div>
+        <div className="menu-items">
+          <div className="menu-links">
+            <div className="menu-item">
+              <Link to={"/"}>Home</Link>
+            </div>
+            <div className="menu-item">
+              <Link to={"/templates"}>Templates</Link>
+            </div>
+            <div className="menu-item">
+              <Link to={"/discover"}>Discover</Link>
+            </div>
+            <div className="menu-item">
+              <Link to={"/learn"}>Learn</Link>
+            </div>
+          </div>
+          <div className="menu-auth">
+            <div className="menu-item">
+              <Link to={"/login"}>Log in</Link>
+            </div>
+            <div className="menu-item">
+              <Link to={"/register"}>Sign up free</Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
