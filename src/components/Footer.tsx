203bd76e8ad2b80a8709dbefa4d9b544fc7dbbca
andrejkoller <andrejkoller@outlook.com>
2025-04-27 13:59:05 +0200

    fix: Update footer height for improved structure and accessibility

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -19,93 +19,91 @@ const Footer = () => {
   };
 
   return (
-    <div className="footer">
-      <div className="footer-container">
-        <div className="footer-content">
-          <div className="footer-content-top">
+    <div className="footer-container">
+      <div className="footer-content">
+        <div className="footer-content-top">
+          <div className="footer-content-item">
+            <div className="footer-content-item-title">
+              <h2>Jumpstart your corner of the internet today</h2>
+            </div>
             <div className="footer-content-item">
-              <div className="footer-content-item-title">
-                <h2>Jumpstart your corner of the internet today</h2>
+              <div className="footer-content-item-input">
+                <Input
+                  id="username"
+                  name="username"
+                  type="text"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  placeholder="linksheet/yourname"
+                />
               </div>
-              <div className="footer-content-item">
-                <div className="footer-content-item-input">
-                  <Input
-                    id="username"
-                    name="username"
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    placeholder="linksheet/yourname"
-                  />
-                </div>
-                <div className="footer-content-item-button">
-                  <Button variant={"solid"} onClick={handleClaimLinksheet}>
-                    Claim your Linksheet
-                  </Button>
-                </div>
+              <div className="footer-content-item-button">
+                <Button variant={"solid"} onClick={handleClaimLinksheet}>
+                  Claim your Linksheet
+                </Button>
               </div>
             </div>
           </div>
-          <div className="footer-content-bottom">
-            <div className="footer-content-item">
-              <div className="footer-content-item-wrapper">
-                <div>
-                  <h3>Company</h3>
-                </div>
-                <div className="footer-content-item-links">
-                  <Link to={"/"}>About</Link>
-                  <Link to={"/"}>Careers</Link>
-                  <Link to={"/"}>Blog</Link>
-                  <Link to={"/"}>Press</Link>
-                  <Link to={"/"}>Brand</Link>
-                </div>
+        </div>
+        <div className="footer-content-bottom">
+          <div className="footer-content-item">
+            <div className="footer-content-item-wrapper">
+              <div>
+                <h3>Company</h3>
               </div>
-              <div className="footer-content-item-wrapper">
-                <div>
-                  <h3>Support</h3>
-                </div>
-                <div className="footer-content-item-links">
-                  <a href="mailto:andrejkoller@outlook.com">Contact Us</a>
-                  <Link to={"/"}>Help Center</Link>
-                  <Link to={"/"}>Status</Link>
-                </div>
-              </div>
-              <div className="footer-content-item-wrapper">
-                <div>
-                  <h3>Trust & Legal</h3>
-                </div>
-                <div className="footer-content-item-links">
-                  <Link to={"/"}>Privacy Policy</Link>
-                  <Link to={"/"}>Terms of Service</Link>
-                  <Link to={"/"}>Cookie Policy</Link>
-                </div>
+              <div className="footer-content-item-links">
+                <Link to={"/"}>About</Link>
+                <Link to={"/"}>Careers</Link>
+                <Link to={"/"}>Blog</Link>
+                <Link to={"/"}>Press</Link>
+                <Link to={"/"}>Brand</Link>
               </div>
             </div>
-            <div className="footer-content-item">
-              <div className="footer-content-item-wrapper-register-login">
-                <Link to={"/login"}>Log in</Link>
-                <Link to={"/register"}>Sign up free</Link>
-              </div>
+            <div className="footer-content-item-wrapper">
               <div>
-                <a href="https://www.youtube.com/@AndrejKoller" target="_blank">
-                  <i className="fa-brands fa-youtube"></i>
-                </a>
-                <a href="https://x.com/andrejkoller" target="_blank">
-                  <i className="fa-brands fa-x-twitter"></i>
-                </a>
-                <a
-                  href="https://www.facebook.com/andrej.koller.18"
-                  target="_blank"
-                >
-                  <i className="fa-brands fa-facebook"></i>
-                </a>
-                <a
-                  href="https://www.instagram.com/andrejkollerofficial"
-                  target="_blank"
-                >
-                  <i className="fa-brands fa-instagram"></i>
-                </a>
+                <h3>Support</h3>
               </div>
+              <div className="footer-content-item-links">
+                <a href="mailto:andrejkoller@outlook.com">Contact Us</a>
+                <Link to={"/"}>Help Center</Link>
+                <Link to={"/"}>Status</Link>
+              </div>
+            </div>
+            <div className="footer-content-item-wrapper">
+              <div>
+                <h3>Trust & Legal</h3>
+              </div>
+              <div className="footer-content-item-links">
+                <Link to={"/"}>Privacy Policy</Link>
+                <Link to={"/"}>Terms of Service</Link>
+                <Link to={"/"}>Cookie Policy</Link>
+              </div>
+            </div>
+          </div>
+          <div className="footer-content-item">
+            <div className="footer-content-item-wrapper-register-login">
+              <Link to={"/login"}>Log in</Link>
+              <Link to={"/register"}>Sign up free</Link>
+            </div>
+            <div>
+              <a href="https://www.youtube.com/@AndrejKoller" target="_blank">
+                <i className="fa-brands fa-youtube"></i>
+              </a>
+              <a href="https://x.com/andrejkoller" target="_blank">
+                <i className="fa-brands fa-x-twitter"></i>
+              </a>
+              <a
+                href="https://www.facebook.com/andrej.koller.18"
+                target="_blank"
+              >
+                <i className="fa-brands fa-facebook"></i>
+              </a>
+              <a
+                href="https://www.instagram.com/andrejkollerofficial"
+                target="_blank"
+              >
+                <i className="fa-brands fa-instagram"></i>
+              </a>
             </div>
           </div>
         </div>
